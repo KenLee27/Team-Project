37b7dc6ba1a13dafeaae55f6a0037bafb1bae804
TKrua <twen2ndvie@gmail.com>
2024-09-07 01:44:50 +0900

    인벤토리 개발일지(3)
    이제 무슨 무기 들고 있는지 UI상으로 확인 가능함 ㅇㅇ

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
@@ -6,13 +7,20 @@ public class InventoryManager : MonoBehaviour
     public GameObject falchionPrefab;
     public GameObject daggerPrefab;
 
+    public Image weaponImage; // 현재 무기 아이콘을 표시할 UI 이미지
+    public Sprite axeSprite; // 각 무기의 스프라이트를 설정합니다
+    public Sprite falchionSprite;
+    public Sprite daggerSprite;
+
     private GameObject[] weaponPrefabs;
+    private Sprite[] weaponSprites; // 각 무기의 스프라이트를 저장하는 배열
     private int currentWeaponIndex = 0;
     private GameObject currentWeapon;
 
     void Start()
     {
         weaponPrefabs = new GameObject[] { axePrefab, falchionPrefab, daggerPrefab };
+        weaponSprites = new Sprite[] { axeSprite, falchionSprite, daggerSprite };
         EquipCurrentWeapon();
     }
 
@@ -28,25 +36,18 @@ public class InventoryManager : MonoBehaviour
     {
         if (currentWeapon != null)
         {
-            Debug.Log("Destroying: " + currentWeapon.name);
             Destroy(currentWeapon);
         }
 
-        // 올바른 자식 오브젝트 인스턴스화(기본적으로 prefab 계층 구조에서 첫번째 자식 사용)
-        GameObject weaponToInstantiate = weaponPrefabs[currentWeaponIndex].transform.GetChild(0).gameObject;
-
-        currentWeapon = Instantiate(weaponToInstantiate);
-
+        currentWeapon = Instantiate(weaponPrefabs[currentWeaponIndex].transform.GetChild(0).gameObject);
         currentWeapon.name = weaponPrefabs[currentWeaponIndex].name + "_Instance";
+
+        UpdateWeaponUI();
 
         WeaponAttachment attachScript = currentWeapon.GetComponent<WeaponAttachment>();
         if (attachScript == null)
         {
             Debug.LogWarning("WeaponAttachment script not found on " + currentWeapon.name);
-        }
-        else
-        {
-            Debug.Log("WeaponAttachment script successfully attached to " + currentWeapon.name);
         }
     }
 
@@ -54,5 +55,13 @@ public class InventoryManager : MonoBehaviour
     {
         currentWeaponIndex = (currentWeaponIndex + 1) % weaponPrefabs.Length;
         EquipCurrentWeapon();
+    }
+
+    private void UpdateWeaponUI()
+    {
+        if (weaponImage != null)
+        {
+            weaponImage.sprite = weaponSprites[currentWeaponIndex];
+        }
     }
 }
