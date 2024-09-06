6d9c5f6b0408b5a25b327fff00f2964009181942
TKrua <twen2ndvie@gmail.com>
2024-09-07 00:36:04 +0900

    인벤토리 개발일지 (1)
    이제 프리팹에 무기 세개 추가...
    
    각각의 무기들은 그냥 맵 아무데나 배치해두면 플레이어의 손으로 자동으로 장착. (초기 인벤토리 장비 스위칭 기능 구현)

---
+++ Assets/Scripts/WeaponAttachment.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttachment : MonoBehaviour
+{
+    private Transform handTransform;
+    private Vector3 positionOffset = new Vector3(0.1210944f, -0.02749907f, 0.07406192f);
+    private Vector3 rotationOffset = new Vector3(39.112f, -53.796f, -70);
+
+    void Start()
+    {
+        SetHandTransform();
+        AttachWeapon();
+    }
+
+    void SetHandTransform()
+    {
+        handTransform = GameObject.Find("Player/root/pelvis/spine_01/spine_02/spine_03/clavicle_r/upperarm_r/lowerarm_r/hand_r").transform;
+
+        if (handTransform == null)
+        {
+            Debug.LogError("Hand Transform could not be found. Please check the hierarchy path.");
+        }
+    }
+
+    void AttachWeapon()
+    {
+        if (handTransform != null)
+        {
+            transform.SetParent(handTransform);
+            transform.localPosition = positionOffset;
+            transform.localRotation = Quaternion.Euler(rotationOffset);
+        }
+        else
+        {
+            Debug.LogWarning("Hand Transform is not assigned in WeaponAttachment.");
+        }
+    }
+}
